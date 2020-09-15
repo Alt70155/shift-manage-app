dd01e7a974b23f9f9f4ae4fec6884083ad79ee92
chika <chika1086@gmail.com>
2020-09-15 14:02:56 +0900

    fakerとseedsの追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,55 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Fakerをコンソールで試す場合はテスト環境で試す
+# rails c -e test
+
+# 管理者
+User.create!(
+  email: 'admin@gmail.com',
+  password: 'password',
+  admin: true,
+  first_name: '太郎',
+  last_name: '田中',
+  available_time_start: '8:00',
+  available_time_end: '17:00',
+  available_days: 5,
+  confirmed_at: Time.now
+)
+
+# 正社員・パートレベルの勤務時間、出勤日数
+s_available_days = [4, 5]
+15.times do |_|
+  last_name = Faker::Name.last_name
+  first_name = Faker::Name.first_name
+  email = Faker::Internet.email
+
+  User.create!(
+    email: email,
+    password: 'password',
+    first_name: first_name,
+    last_name: last_name,
+    available_time_start: '8:00',
+    available_time_end: '17:00',
+    available_days: s_available_days.sample,
+    confirmed_at: Time.now # deviseのメール認証を通過させるフィールド
+  )
+end
+
+# 学生レベルの勤務時間
+start_time_list = ['17:00', '18:00', '19:00']
+end_time_list = ['21:00', '22:00', '23:00']
+g_available_days = [2, 3, 4, 5]
+15.times do |_|
+  last_name = Faker::Name.last_name
+  first_name = Faker::Name.first_name
+  email = Faker::Internet.email
+
+  User.create!(
+    email: email,
+    password: 'password',
+    first_name: first_name,
+    last_name: last_name,
+    available_time_start: start_time_list.sample,
+    available_time_end: end_time_list.sample,
+    available_days: g_available_days.sample,
+    confirmed_at: Time.now # deviseのメール認証を通過させるフィールド
+  )
+end
