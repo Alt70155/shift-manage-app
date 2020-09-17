f6343f954d22bb198ba7b76c6c049df801553821
chika <chika1086@gmail.com>
2020-09-17 10:45:29 +0900

    バリデーションの追加

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,3 +1,7 @@
 class Request < ApplicationRecord
   belongs_to :user
+
+  validates :user, presence: true
+  validates :date, presence: true, uniqueness: { scope: :user_id } # 複合ユニーク制約
+  validates :possible, presence: true
 end
--- app/models/store.rb
+++ app/models/store.rb
@@ -1,3 +1,12 @@
 class Store < ApplicationRecord
   belongs_to :user
+
+  validates :user, presence: true
+  validates :emp_per_day_number,  presence: true, numericality: { greater_than: 0 }
+  validates :business_start_time, presence: true
+  validates :business_end_time,   presence: true
+  validates :regular_holiday,     presence: true, numericality: {
+    greater_than_or_equal_to: 0,
+    less_than_or_equal_to:    7
+  }
 end
