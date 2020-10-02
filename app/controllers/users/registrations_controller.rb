162c3f909e1a484aad5f094a35e90feb325aca51
chika <chika1086@gmail.com>
2020-10-02 14:33:38 +0900

    基本情報の変更を一般ユーザーができるよう変更

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  before_action :logged_in_admin
+  before_action :logged_in_admin, only: [:new, :create]
   before_action :configure_sign_up_params, only: [:create]
   before_action :configure_account_update_params, only: [:update]
   # 以下でdeviseの設定を上書きし、ログイン状態でもサインイン・アップできるようにする
-  prepend_before_action :require_no_authentication, only: [:cancel]
-  prepend_before_action :authenticate_scope!, only: [:update, :destroy]
+  # prepend_before_action :require_no_authentication, only: [:cancel]
+  # prepend_before_action :authenticate_scope!, only: [:update, :destroy]
 
   # GET /resource/sign_up
   def new
@@ -29,23 +29,23 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   # PUT /resource
-  def update
-    self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
-    prev_unconfirmed_email = resource.unconfirmed_email if resource.respond_to?(:unconfirmed_email)
+  # def update
+    # self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
+    # prev_unconfirmed_email = resource.unconfirmed_email if resource.respond_to?(:unconfirmed_email)
 
-    resource_updated = update_resource(resource, account_update_params)
-    yield resource if block_given?
-    if resource_updated
-      set_flash_message_for_update(resource, prev_unconfirmed_email)
-      bypass_sign_in resource, scope: resource_name if sign_in_after_change_password?
+    # resource_updated = update_resource(resource, account_update_params)
+    # yield resource if block_given?
+    # if resource_updated
+    #   set_flash_message_for_update(resource, prev_unconfirmed_email)
+    #   bypass_sign_in resource, scope: resource_name if sign_in_after_change_password?
 
-      respond_with resource, location: after_update_path_for(resource)
-    else
-      clean_up_passwords resource
-      set_minimum_password_length
-      respond_with resource
-    end
-  end
+    #   respond_with resource, location: after_update_path_for(resource)
+    # else
+    #   clean_up_passwords resource
+    #   set_minimum_password_length
+    #   respond_with resource
+    # end
+  # end
 
   # DELETE /resource
   def destroy
