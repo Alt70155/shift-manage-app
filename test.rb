1646430c327f9292775036fb7c2b5aefd42651e8
chika <chika1086@gmail.com>
2020-10-20 14:29:19 +0900

    シフト生成処理を分離

--- test.rb
+++ test.rb
@@ -1,32 +1,25 @@
-# 勤務可能時間が正社員レベルの人とバイトレベルの人を分ける
-# DB内部ではタイムゾーンがUTCのため、普通の時刻で検索すると出てこないので、Tokyo→UTCに変更する必要がある
-regular_employee_time_in = convert_to_utc('08:00:00')
+regular_time_in = convert_to_utc('08:00:00')
 evening_time_in = convert_to_utc('17:00:00')
 midnight_time_in = convert_to_utc('21:00:00')
 
-regular_employees  = User.where(available_time_start: regular_employee_time_in).to_a
-evening_employees  = User.where(available_time_start: evening_time_in).to_a
-midnight_employees = User.where(available_time_start: midnight_time_in).to_a
+regular_employees  = User.where(available_time_start: regular_time_in)
+evening_employees  = User.where(available_time_start: evening_time_in)
+midnight_employees = User.where(available_time_start: midnight_time_in)
 
 store = User.find_by(admin: true).store
-emp_per_day_number = store.emp_per_day_number
-$worked_counter_list = [0] * (User.count + 1)
+$weekly_worked_counter_list = [0] * (User.count + 1)
 
-# 上のリストからランダムに5人ずつ取得してタイムテーブルを作成する
-###
-# TODO:定休日を抜かす
-##
 days_in_next_month.times do |day|
   day += 1
-  # 一日の従業員をランダムに抽出する
-  random_emp_list = []
-  random_emp_list.append(regular_employees.sample(emp_per_day_number))
-                 .append(evening_employees.sample(emp_per_day_number))
-                 .append(midnight_employees.sample(emp_per_day_number))
-                 .flatten!
+  # 一日の従業員を抽出する
+  random_regular_emp_list  = extract_employees(day, regular_employees)
+  random_evening_emp_list  = extract_employees(day, evening_employees)
+  random_midnight_emp_list = extract_employees(day, midnight_employees)
 
   # DB登録用の日付文字列を作成（来月の年と月の文字列を作成）
   working_days = Date.today.next_month.strftime('%Y-%m') + "-#{day}"
+
+  random_emp_list = random_regular_emp_list + random_evening_emp_list + random_midnight_emp_list
 
   random_emp_list.each do |emp|
     emp.shifts.create!(working_days: working_days)
@@ -34,33 +27,74 @@ days_in_next_month.times do |day|
 
   # 一週間経ったらリストをリセットする
   if (day % 7).zero?
-    regular_employees  = User.where(available_time_start: regular_employee_time_in).to_a
-    evening_employees  = User.where(available_time_start: evening_time_in).to_a
-    midnight_employees = User.where(available_time_start: midnight_time_in).to_a
-    $worked_counter_list = [0] * (User.count + 1)
+    regular_employees  = User.where(available_time_start: regular_time_in)
+    evening_employees  = User.where(available_time_start: evening_time_in)
+    midnight_employees = User.where(available_time_start: midnight_time_in)
+    $weekly_worked_counter_list = [0] * (User.count + 1)
   else
-    regular_employees  = work_days_management(regular_employees)
-    evening_employees  = work_days_management(evening_employees)
-    midnight_employees = work_days_management(midnight_employees)
+    regular_employees  = work_days_management(regular_employees, random_regular_emp_list)
+    evening_employees  = work_days_management(evening_employees, random_evening_emp_list)
+    midnight_employees = work_days_management(midnight_employees, random_midnight_emp_list)
   end
 end
 
-def work_days_management(employees_list)
-  index_list = []
+# 出勤分布確認コード
+# @temp_list = Array.new(4) { Array.new(User.count + 1, 0) }
+
+# @temp_list.each_with_index do |week_list, i|
+#   i *= 7
+
+#   7.times do |j|
+#     day = i + j
+
+#     @x[day].each do |emp|
+#       week_list[emp.id] += 1
+#     end
+#   end
+# end
+
+# その日に働く従業員を抽出する処理
+def extract_employees(day, employees_list)
+  store = User.find_by(admin: true).store
+  emp_per_day_number = store.emp_per_day_number
+  work_date = Date.today.next_month.strftime('%Y-%m') + "-#{day}"
+
+  # day日の出勤希望日がある人を抽出
+  hope_work_list = employees_list.select do |emp|
+    emp.requests.where(possible: true, date: work_date).present?
+  end
+
+  go_to_work_list = hope_work_list.sample(emp_per_day_number)
+
+  # 希望者が必要な人数より少なかった場合、ランダムで追加する
+  if go_to_work_list.length < emp_per_day_number
+    # day日の出勤希望がfalseの人を排除し、出勤可能な人のみを集める
+    possible_list = employees_list.reject do |emp|
+      emp.requests.where(possible: false, date: work_date).present?
+    end
+
+    diff_num = emp_per_day_number - go_to_work_list.length
+    go_to_work_list.append(possible_list.sample(diff_num)).flatten!
+  end
+
+  go_to_work_list
+end
+
+# 出勤カウントを増やし、出勤した従業員が一週間で出勤できる日数を
+# 超えていた場合に従業員リストから除外する処理
+def work_days_management(employees_list, random_emp_list)
+  delete_list = []
   # 抽出した従業員の出勤日数を1増やす
-  employees_list.each_with_index do |emp, i|
-    $worked_counter_list[emp.id] += 1
+  random_emp_list.each do |emp|
+    emp_id = emp.id
+    $weekly_worked_counter_list[emp_id] += 1
 
     # 1増やした後の出勤日数が出勤可能日数と同じになった場合、
-    # 削除すべき添字をindex_listに記録する
-    index_list.append(i) if $worked_counter_list[emp.id] == emp.available_days
+    # 削除すべき添字をdelete_listに記録する
+    delete_list.append(emp_id) if $weekly_worked_counter_list[emp_id] == emp.available_days
   end
-  # 従業員リストから削除する
-  index_list.each do |i|
-    employees_list.delete_at(i)
-  end
-
-  employees_list
+  # 従業員リストから除外する
+  employees_list.reject { |emp| delete_list.include?(emp.id) }
 end
 
 # 来月の日数を取得
@@ -73,21 +107,4 @@ def convert_to_utc(str)
   # 文字列をTokyoの時刻オブジェクトにした後、UTCに変更する
   # 一度TokyoにしないとUTC時刻になってしまうので注意
   str.in_time_zone('Tokyo').in_time_zone('UTC')
-end
-
-
-#### 分布の確認
-temp_list = [0] * (User.count + 1)
-
-4.times do |day|
-  day += 1
-  start_day = Date.today.next_month.strftime('%Y-%m') + "-#{day}"
-  end_day   = Date.today.next_month.strftime('%Y-%m') + "-#{day * 7}"
-
-  shift_list = Shift.where(working_days: start_day..end_day)
-  shift_list.each do |shift|
-    temp_list[shift.user.id] += 1
-  end
-
-  shift_list.
 end
