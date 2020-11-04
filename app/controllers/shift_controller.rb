c7261fbdc3f8c11002a5c5523c4bc806323f55fe
chika <chika1086@gmail.com>
2020-11-05 00:14:42 +0900

    シフト表示の為のデータの抽出

--- app/controllers/shift_controller.rb
+++ app/controllers/shift_controller.rb
@@ -5,9 +5,16 @@ class ShiftController < ApplicationController
   def index
     @request_list = current_user.requests.all.order(:date)
     # 今月のシフト・来月のシフトの両方を日にちによって表示する/しないを判定する必要あり
-    next_month = Date.today.next_month
-    @shift_list = Shift.where(working_days: next_month.beginning_of_month..next_month.end_of_month)
-    @user_list = User.all
+    @next_month = Date.today.next_month
+    next_month_shift_list = Shift.where(working_days: @next_month.beginning_of_month..@next_month.end_of_month)
+
+    @grouped_next_month_shift_list = []
+    days_in_next_month = Time.days_in_month(@next_month.month, @next_month.year)
+    days_in_next_month.times do |i|
+      day = @next_month.beginning_of_month.strftime('%Y-%m-') + (i + 1).to_s
+      @grouped_next_month_shift_list.append(next_month_shift_list.where(working_days: day))
+    end
+
   end
 
   def profile
