d6ee438fedd7b1d365af73b1c747cbab7f7f9fed
chika <chika1086@gmail.com>
2020-11-10 11:58:25 +0900

    月のシフト生成処理をモデルに移譲

--- app/controllers/shift_controller.rb
+++ app/controllers/shift_controller.rb
@@ -5,15 +5,12 @@ class ShiftController < ApplicationController
   def index
     @request_list = current_user.requests.all.order(:date)
     # 今月のシフト・来月のシフトの両方を日にちによって表示する/しないを判定する必要あり
+    # 今月のシフト
+    @this_month = Date.today
+    @this_month_shift_list = Shift.create_month_list(@this_month)
+    # 来月のシフト
     @next_month = Date.today.next_month
-    next_month_shift_list = Shift.where(working_days: @next_month.beginning_of_month..@next_month.end_of_month)
-
-    @grouped_next_month_shift_list = []
-    days_in_next_month = Time.days_in_month(@next_month.month, @next_month.year)
-    days_in_next_month.times do |i|
-      day = @next_month.beginning_of_month.strftime('%Y-%m-') + (i + 1).to_s
-      @grouped_next_month_shift_list.append(next_month_shift_list.where(working_days: day))
-    end
+    @next_month_shift_list = Shift.create_month_list(@next_month)
 
   end
 
@@ -23,4 +20,17 @@ class ShiftController < ApplicationController
   def users
     @user_list = User.all
   end
+
+  private
+
+    def create_shift_list(date)
+      shift_list = Shift.where(working_days: date.beginning_of_month..date.end_of_month)
+
+      grouped_shift_list = []
+      days_num = Time.days_in_month(date.month, date.year)
+      days_num.times do |i|
+        day = date.beginning_of_month.strftime('%Y-%m-') + (i + 1).to_s
+        grouped_shift_list.append(shift_list.where(working_days: day))
+      end
+    end
 end
--- app/models/shift.rb
+++ app/models/shift.rb
@@ -1,3 +1,13 @@
 class Shift < ApplicationRecord
   belongs_to :user
+
+  def self.create_month_list(date)
+    shift_list = where(working_days: date.beginning_of_month..date.end_of_month)
+    days_num = Time.days_in_month(date.month, date.year)
+
+    (1..days_num).map do |i|
+      day = date.beginning_of_month.strftime('%Y-%m-') + i.to_s
+      shift_list.where(working_days: day)
+    end
+  end
 end
