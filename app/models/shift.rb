b4d53af47ffe277f54ce95598087565ae470a018
chika <chika1086@gmail.com>
2020-11-12 10:06:02 +0900

    勤務時間でソートする処理を追加

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -5,9 +5,15 @@ class Shift < ApplicationRecord
     shift_list = where(working_days: date.beginning_of_month..date.end_of_month)
     days_num = Time.days_in_month(date.month, date.year)
 
-    (1..days_num).map do |i|
+    days_shift_list = (1..days_num).map do |i|
       day = date.beginning_of_month.strftime('%Y-%m-') + i.to_s
       shift_list.where(working_days: day)
+    end
+
+    days_shift_list.map do |list|
+      list.sort do |a, b|
+        a.user.available_time_start <=> b.user.available_time_start
+      end
     end
   end
 end
